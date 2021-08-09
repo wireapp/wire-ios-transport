208c2f7d810b8918d4181f097d0763f211894ee0
Bill Chan <billypchan@gmail.com>
2021-08-09 20:36:31 +0200

    undo

--- Tests/Source/URLSession/ZMTaskIdentifierTests.m
+++ Tests/Source/URLSession/ZMTaskIdentifierTests.m
@@ -21,7 +21,6 @@
 @import WireTesting;
 @import OCMock;
 #import "ZMTaskIdentifier.h"
-#import <WireTransport/WireTransport-Swift.h>
 
 
 @interface ZMTaskIdentifierTests : ZMTBaseTest
@@ -58,6 +57,36 @@
     XCTAssertNotEqualObjects(first, second);
     XCTAssertNotEqualObjects(first, third);
     XCTAssertNotEqualObjects(second, third);
+}
+
+- (void)testThatItCanBeSerializedAndDeserializedFromAndToNSData {
+    // given
+    ZMTaskIdentifier *sut = [ZMTaskIdentifier identifierWithIdentifier:46 sessionIdentifier:@"foreground-session"];
+    XCTAssertNotNil(sut);
+    
+    // when
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:sut];
+    XCTAssertNotNil(data);
+    
+    // then
+    ZMTaskIdentifier *deserializedSut = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+    XCTAssertNotNil(deserializedSut);
+    XCTAssertEqualObjects(deserializedSut, sut);
+}
+
+- (void)testThatItCanBeInitializedFromDataAndReturnsTheCorrectData {
+    // given
+    ZMTaskIdentifier *sut = [ZMTaskIdentifier identifierWithIdentifier:42 sessionIdentifier:@"foreground-session"];
+    XCTAssertNotNil(sut);
+    
+    // when
+    NSData *data = sut.data;
+    XCTAssertNotNil(data);
+    
+    // then
+    ZMTaskIdentifier *deserializedSut = [ZMTaskIdentifier identifierFromData:data];
+    XCTAssertNotNil(deserializedSut);
+    XCTAssertEqualObjects(deserializedSut, sut);
 }
 
 @end
